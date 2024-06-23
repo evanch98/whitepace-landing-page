3cf49d6a04bd90a84a96304035d3047ff999b47b
Kyaw Thu <evanch98@gmail.com>
2024-06-23 20:48:56 +0800

    exporting * from the nav folder

---
+++ components/molecules/nav/index.ts
@@ -0,0 +1,6 @@
+export * from "./navbar";
+export * from "./nav-link";
+export * from "./nav-menu";
+export * from "./nav-products";
+export * from "./nav-solutions";
+export * from "./nav-resources";
