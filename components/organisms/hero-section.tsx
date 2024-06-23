bc47121b947411b541f3a8ef61131ac30081608b
Kyaw Thu <evanch98@gmail.com>
2024-06-24 00:14:16 +0800

    adding a new section

---
+++ components/organisms/hero-section.tsx
@@ -0,0 +1,26 @@
+import { Button, Container } from "@/components/atoms";
+import { HeroBackground, HeroImage, HeroText } from "@/components/molecules";
+import { ArrowRight } from "lucide-react";
+import Image from "next/image";
+
+export const HeroSection = () => {
+  return (
+    <div className="relative overflow-hidden">
+      <Container className="bg-secondary text-white h-full flex flex-col items-center justify-center gap-y-[100px] lg:flex-row py-[80px] md:pt-[140px] md:pb-[100px] lg:py-[140px] xl:py-[100px] 2xl:py-[140px]">
+        <div className="flex items-center lg:items-start flex-col gap-y-[60px]">
+          <HeroText />
+          <div className="z-10">
+            <Button size="with-icon">
+              Try Whitepace free
+              <ArrowRight className="w-[10px] h-[10px]" />
+            </Button>
+          </div>
+        </div>
+        <HeroImage />
+      </Container>
+      <div className="absolute top-1/2 -translate-y-[440px] md:-translate-y-1/2 left-1/2 -translate-x-1/2 w-full h-[633px] md:h-[693px] lg:h-[422px] xl:h-[433px] 2xl:h-[547px]">
+        <HeroBackground />
+      </div>
+    </div>
+  );
+};
