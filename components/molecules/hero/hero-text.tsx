3a0d40c5ab96d8773998f7f35fa988046ef1d0ff
Kyaw Thu <evanch98@gmail.com>
2024-06-24 00:12:21 +0800

    adding a new component

---
+++ components/molecules/hero/hero-text.tsx
@@ -0,0 +1,11 @@
+export const HeroText = () => {
+  return (
+    <div className="flex flex-col gap-y-6 z-10">
+      <h1 className="text-h4 md:text-h3 lg:text-h2 font-bold text-center lg:text-left">Get More Done with whitepace</h1>
+      <p className="text-p2 text-center lg:text-left">
+        Project management software that enables your teams to collaborate,
+        plan, analyze, and manage everyday tasks.
+      </p>
+    </div>
+  );
+};
