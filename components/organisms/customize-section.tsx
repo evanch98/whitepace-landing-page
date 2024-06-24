e20c1b492968a8e98037fbdeba08f6c46b888015
Kyaw Thu <evanch98@gmail.com>
2024-06-25 00:53:14 +0800

    adding a new section

---
+++ components/organisms/customize-section.tsx
@@ -0,0 +1,22 @@
+import { Button, Container } from "@/components/atoms";
+import { HeroImage, Section3Text } from "@/components/molecules";
+import { ArrowRight } from "lucide-react";
+
+export const CustomizeSection = () => {
+  return (
+    <div className="w-full">
+      <Container className="text-white bg-secondary lg:justify-center lg:gap-x-[60px] 2xl:gap-x-[98px]">
+        <div className="flex items-center lg:items-start flex-col gap-y-[60px]">
+          <Section3Text />
+          <div className="z-10">
+            <Button size="with-icon">
+              Let&apos; Go
+              <ArrowRight className="w-4 h-4" />
+            </Button>
+          </div>
+        </div>
+        <HeroImage imgSrc="/assets/customize.svg" />
+      </Container>
+    </div>
+  );
+};
