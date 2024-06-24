91ebd1d55b9c6333f26df896a44169f24ef44a10
Kyaw Thu <evanch98@gmail.com>
2024-06-25 00:25:18 +0800

    adding a new s ection

---
+++ components/organisms/work-together-section.tsx
@@ -0,0 +1,22 @@
+import { Button, Container } from "@/components/atoms";
+import { CircularAvatars, Section2Text } from "@/components/molecules";
+import { ArrowRight } from "lucide-react";
+
+export const WorkTogetherSection = () => {
+  return (
+    <div className="w-full">
+      <Container className="text-accent-black xl:justify-center xl:gap-x-[160px] 2xl:gap-x-[100px]">
+        <CircularAvatars />
+        <div className="flex items-center lg:items-start flex-col gap-y-[60px]">
+          <Section2Text />
+          <div className="z-10">
+            <Button size="with-icon">
+              Try it now
+              <ArrowRight className="w-4 h-4" />
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
