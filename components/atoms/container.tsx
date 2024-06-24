1e3fc83759f45a85193cfd96ad24b39cb54e26e5
Kyaw Thu <evanch98@gmail.com>
2024-06-24 20:09:16 +0800

    adding more default classNames

--- components/atoms/container.tsx
+++ components/atoms/container.tsx
@@ -8,7 +8,10 @@ interface IContainer {
 export const Container = ({ children, className }: IContainer) => {
   return (
     <div
-      className={cn("w-full px-4 md:px-5 lg:px-8 2xl:px-[220px]", className)}
+      className={cn(
+        "w-full px-4 md:px-5 lg:px-8 2xl:px-[220px] h-full flex flex-col items-center justify-center gap-y-[100px] lg:flex-row py-[80px] md:pt-[140px] md:pb-[100px] lg:py-[140px] xl:py-[100px] 2xl:py-[140px]",
+        className
+      )}
     >
       {children}
     </div>
