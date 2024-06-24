285b6f76ad234966cb44582c5fc628972bdf201a
Kyaw Thu <evanch98@gmail.com>
2024-06-24 19:16:56 +0800

    updating to add the MobileNav

--- components/organisms/navbar.tsx
+++ components/organisms/navbar.tsx
@@ -1,29 +1,29 @@
 import { Button, Container, Logo } from "@/components/atoms";
-import { useMediaQuery } from "@/hooks";
-import { NavMenu } from "@/components/molecules";
+import { MobileNav, NavMenu } from "@/components/molecules";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 
 export const Navbar = () => {
-  const largeScreen = useMediaQuery("(min-width: 1440px)");
-
   return (
     <Container className="bg-secondary">
       <nav className="py-4 text-white flex items-center justify-between">
         <Link href="/">
           <Logo className="w-[134px] h-[24px] md:w-[191px] md:h-[34px]" />
         </Link>
-        {largeScreen && (
-          <div className="flex items-center justify-center gap-x-[60px] z-50">
-            <NavMenu />
-            <div className="flex gap-x-6">
-              <Button variant="secondary">Login</Button>
-              <Button size="with-icon">
-                Try Whitepace free <ArrowRight />
-              </Button>
+        <div className="flex items-center justify-center gap-x-[60px] z-50">
+          <NavMenu className="hidden xl:flex" />
+          <div className="flex gap-x-6">
+            <Button variant="secondary" className="hidden lg:flex">
+              Login
+            </Button>
+            <Button size="with-icon" className="hidden lg:flex">
+              Try Whitepace free <ArrowRight />
+            </Button>
+            <div className="flex xl:hidden">
+              <MobileNav />
             </div>
           </div>
-        )}
+        </div>
       </nav>
     </Container>
   );
