3f79a5f18af8d0b369a5fd07d0ba3097dfbbaeb1
Kyaw Thu <evanch98@gmail.com>
2024-06-24 20:08:56 +0800

    updating the style

--- components/organisms/navbar.tsx
+++ components/organisms/navbar.tsx
@@ -5,26 +5,24 @@ import Link from "next/link";
 
 export const Navbar = () => {
   return (
-    <Container className="bg-secondary">
-      <nav className="py-4 text-white flex items-center justify-between">
-        <Link href="/">
-          <Logo className="w-[134px] h-[24px] md:w-[191px] md:h-[34px]" />
-        </Link>
-        <div className="flex items-center justify-center gap-x-[60px] z-50">
-          <NavMenu className="hidden xl:flex" />
-          <div className="flex gap-x-6">
-            <Button variant="secondary" className="hidden lg:flex">
-              Login
-            </Button>
-            <Button size="with-icon" className="hidden lg:flex">
-              Try Whitepace free <ArrowRight />
-            </Button>
-            <div className="flex xl:hidden">
-              <MobileNav />
-            </div>
+    <nav className="py-4 text-white flex items-center justify-between w-full px-4 md:px-5 lg:px-8 2xl:px-[220px] bg-secondary">
+      <Link href="/">
+        <Logo className="w-[134px] h-[24px] md:w-[191px] md:h-[34px]" />
+      </Link>
+      <div className="flex items-center justify-center gap-x-[60px] z-50">
+        <NavMenu className="hidden xl:flex" />
+        <div className="flex gap-x-6">
+          <Button variant="secondary" className="hidden lg:flex">
+            Login
+          </Button>
+          <Button size="with-icon" className="hidden lg:flex">
+            Try Whitepace free <ArrowRight />
+          </Button>
+          <div className="flex xl:hidden">
+            <MobileNav />
           </div>
         </div>
-      </nav>
-    </Container>
+      </div>
+    </nav>
   );
 };
