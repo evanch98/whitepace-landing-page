c18cc32fdb5858afa74073e14e2250b8da991407
Kyaw Thu <evanch98@gmail.com>
2024-06-25 00:53:25 +0800

    exporting components

--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -2,3 +2,4 @@ export * from "./hero-section";
 export * from "./navbar";
 export * from "./project-management-section";
 export * from "./work-together-section";
+export * from "./customize-section";
