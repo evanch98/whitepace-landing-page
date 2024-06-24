93d9fecbd6f26d3dd4c0b4c92b056416707d6680
Kyaw Thu <evanch98@gmail.com>
2024-06-24 19:14:52 +0800

    updating the style

--- components/molecules/nav/nav-link.tsx
+++ components/molecules/nav/nav-link.tsx
@@ -8,19 +8,16 @@ interface INavLink {
   desc?: string;
 }
 
-export const NavLink = ({
-  icon: Icon,
-  title,
-  className,
-  desc,
-}: INavLink) => {
+export const NavLink = ({ icon: Icon, title, className, desc }: INavLink) => {
   return (
     <div className={cn("flex items-center gap-x-4", className)}>
-      {Icon && <Icon className="h-7 w-7 text-primary flex-shrink-0" />}
+      {Icon && (
+        <Icon className="h-6 w-6 lg:h-7 lg:w-7 text-primary flex-shrink-0" />
+      )}
       <div className="flex flex-col">
-        <h1 className="text-p2">{title}</h1>
+        <h1 className="text-p3 lg:p2">{title}</h1>
         {desc && (
-          <p className="text-p3 text-accent-black/50">{desc}</p>
+          <p className="text-xs lg:text-p3 text-accent-black/50">{desc}</p>
         )}
       </div>
     </div>
