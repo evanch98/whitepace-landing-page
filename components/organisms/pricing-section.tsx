885b519b49a660a6dd7803d649535c1226541769
Kyaw Thu <evanch98@gmail.com>
2024-06-25 23:45:48 +0800

    adding a new section

---
+++ components/organisms/pricing-section.tsx
@@ -0,0 +1,25 @@
+import { Container } from "@/components/atoms";
+import {
+  PricingCard,
+  PricingCarousel,
+  PricingSectionText,
+} from "@/components/molecules";
+import { freePlan, organizationPlan, personalPlan } from "@/content";
+
+export const PricingSection = () => {
+  return (
+    <div className="w-full">
+      <Container className="text-accent-black lg:flex-col items-center justify-center gap-y-[60px]">
+        <PricingSectionText />
+        <div className="hidden lg:flex w-full items-center justify-center gap-x-8">
+          <PricingCard {...freePlan} />
+          <PricingCard isHighlight {...personalPlan} />
+          <PricingCard {...organizationPlan} />
+        </div>
+      </Container>
+      <div className="w-full flex items-center justify-center overflow-hidden lg:hidden">
+        <PricingCarousel />
+      </div>
+    </div>
+  );
+};
