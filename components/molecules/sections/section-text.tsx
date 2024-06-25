1916873932c1f1297f980a70b11503e598a44c2d
Kyaw Thu <evanch98@gmail.com>
2024-06-25 23:44:34 +0800

    adding new text

--- components/molecules/sections/section-text.tsx
+++ components/molecules/sections/section-text.tsx
@@ -33,7 +33,7 @@ export const Section2Text = () => {
           <span className="relative z-50">together</span>
           <div className="absolute flex -bottom-1 left-0 z-10">
             <Stroke
-              className="w-[169px] h-[25px] md:w-[224px] md:h-[31px] lg:w-[266px] lg:h-[24px] xl:w-[298px] xl:h-[28px] flex-shrink-0"
+              className="w-[169px] h-[25px] md:w-[224px] md:h-[21px] lg:w-[266px] lg:h-[24px] xl:w-[298px] xl:h-[28px] flex-shrink-0"
               imgSrc="/assets/stroke-1.svg"
             />
           </div>
@@ -67,6 +67,29 @@ export const Section3Text = () => {
         Customize the app with plugins, custom themes and multiple text editors
         (Rich Text or Markdown). Or create your own scripts and plugins using
         the Extension API.
+      </p>
+    </div>
+  );
+};
+
+export const PricingSectionText = () => {
+  return (
+    <div className="flex flex-col gap-y-6 z-10">
+      <h1 className="text-h4 md:text-h3 lg:text-h2 xl:text-h1 font-bold text-center">
+        Choose Your{" "}
+        <span className="relative">
+          <span className="relative z-50">Plan</span>
+          <div className="absolute flex -bottom-1 md:bottom-0 xl:bottom-2 left-0 z-10">
+            <Stroke
+              className="w-[80px] h-[19px] md:w-[200px] md:h-[19px] lg:w-[200px] lg:h-[19px] xl:w-[205px] xl:h-[19px] flex-shrink-0"
+              imgSrc="/assets/stroke.svg"
+            />
+          </div>
+        </span>
+      </h1>
+      <p className="text-p2 text-center w-full lg:max-w-[979px]">
+        Whether you want to get organized, keep your personal life on track, or
+        boost workplace productivity, Whitepace has the right plan for you.
       </p>
     </div>
   );
