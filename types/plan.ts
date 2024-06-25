e57fc4e9a43e5c764883db2a1bc874c37a9437dc
Kyaw Thu <evanch98@gmail.com>
2024-06-25 23:41:55 +0800

    adding a new type

---
+++ types/plan.ts
@@ -0,0 +1,9 @@
+export const IPlansDefs = ["Free", "Personal", "Organization"] as const;
+export type IPlansType = typeof IPlansDefs[number];
+
+export interface IPlan {
+  plan: IPlansType;
+  price: string;
+  desc: string;
+  benefits: string[];
+}
