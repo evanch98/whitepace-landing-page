135d5ebddcf2a0d2d867a8518232c019442ff128
Kyaw Thu <evanch98@gmail.com>
2024-06-26 16:06:08 +0800

    adding a new section

---
+++ components/organisms/sponsors-section.tsx
@@ -0,0 +1,9 @@
+import { Container } from "@/components/atoms";
+
+export const SponsorsSection = () => {
+  return (
+    <Container>
+      <div className="w-full flex flex-col items-center justify-center gap-y-[60px] py-[80px] md:pt-[140px] md:pb-[100px] lg:py-[140px] xl:py-[100px] 2xl:py-[140px]"></div>
+    </Container>
+  );
+};
