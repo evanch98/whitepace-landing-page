fbb1a15d727b7e6dc546ee3a0b8d64180d2edce6
Kyaw Thu <evanch98@gmail.com>
2024-06-27 00:06:33 +0800

    adding a new section

---
+++ components/organisms/favorite-apps-section.tsx
@@ -0,0 +1,7 @@
+import { Container } from "@/components/atoms";
+
+export const FavoriteAppsSection = () => {
+  return (
+    <Container className="text-white bg-secondary">Favorite Apps</Container>
+  );
+};
