b4f33ca22eb2b9dbefe23352acd5870b30a0bde1
Kyaw Thu <evanch98@gmail.com>
2024-06-27 01:05:41 +0800

    updating the FavoriteAppSection

--- components/organisms/favorite-apps-section.tsx
+++ components/organisms/favorite-apps-section.tsx
@@ -1,7 +1,22 @@
-import { Container } from "@/components/atoms";
+import { Button, Container } from "@/components/atoms";
+import { FavoriteApps, Section6Text } from "@/components/molecules";
+import { ArrowRight } from "lucide-react";
 
 export const FavoriteAppsSection = () => {
   return (
-    <Container className="text-white bg-secondary">Favorite Apps</Container>
+    <Container className="text-white bg-secondary">
+      <div className="flex flex-col lg:flex-row items-center justify-center gap-[100px] py-[80px] md:py-[140px]">
+        <FavoriteApps />
+        <div className="flex items-center lg:items-start flex-col gap-y-[60px]">
+          <Section6Text />
+          <div className="z-10">
+            <Button size="with-icon">
+              Get Started
+              <ArrowRight className="w-4 h-4" />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </Container>
   );
 };
