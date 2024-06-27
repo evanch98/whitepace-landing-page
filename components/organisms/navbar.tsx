427755b5b83dfc4599d39334ee4b633167f05655
Kyaw Thu <evanch98@gmail.com>
2024-06-27 14:53:45 +0800

    updating Navbar

--- components/organisms/navbar.tsx
+++ components/organisms/navbar.tsx
@@ -1,9 +1,12 @@
 import { Button, Container, Logo } from "@/components/atoms";
 import { MobileNav, NavMenu } from "@/components/molecules";
+import { useAttributionModal } from "@/stores";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 
 export const Navbar = () => {
+  const attributionModal = useAttributionModal();
+
   return (
     <Container className="text-white bg-secondary">
       <nav className="py-4 flex items-center justify-between w-full">
@@ -13,10 +16,18 @@ export const Navbar = () => {
         <div className="flex items-center justify-center gap-x-[60px] z-50">
           <NavMenu className="hidden xl:flex" />
           <div className="flex gap-x-6">
-            <Button variant="secondary" className="hidden lg:flex">
+            <Button
+              onClick={() => attributionModal.onOpen()}
+              variant="secondary"
+              className="hidden lg:flex"
+            >
               Login
             </Button>
-            <Button size="with-icon" className="hidden lg:flex">
+            <Button
+              onClick={() => attributionModal.onOpen()}
+              size="with-icon"
+              className="hidden lg:flex"
+            >
               Try Whitepace free <ArrowRight />
             </Button>
             <div className="flex xl:hidden">
